7167e62fd858dd3e0e38915cc6510fee980f3186
Iamasoldier6 <iamasoldiersix@gmail.com>
2016-05-25 00:02:36 +0800

    见 5.24 更新日志

--- SoldierNote/app/src/main/java/com/iamasoldier6/soldiernote/activity/MainActivity.java
+++ SoldierNote/app/src/main/java/com/iamasoldier6/soldiernote/activity/MainActivity.java
@@ -1,12 +1,14 @@
 package com.iamasoldier6.soldiernote.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.iamasoldier6.soldiernote.R;
 
@@ -14,36 +16,56 @@ public class MainActivity extends ActionBarActivity {
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_left);
+        mToolbar = (Toolbar) findViewById(R.id.id_toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
 
         /**
          * 标题文字须在 setSupportActionBar 之前
          */
-        mToolbar.setTitle("新建"); // 设置 Toolbar 标题
         setSupportActionBar(mToolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this, mDrawerLayout, mToolbar,
-                R.string.abc_action_bar_home_description,
-                R.string.abc_action_bar_home_description_format);
-        mDrawerToggle.syncState();
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        setupDrawerContent(mNavigationView);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/*");
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu, this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_drawer, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
